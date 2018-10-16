aa70c2666d52523627717b008108f99243e48bc3
anthonychen1109 <chen.anthony.1109@gmail.com>
2018-10-16 15:11:10 -0400

    rendering companies for logged in uesrs

--- src/components/company/company.js
+++ src/components/company/company.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { get_current_user } from '../profile/actions';
+import { Card } from 'semantic-ui-react';
 
 const mapStateToProps = (state) => {
   return {
@@ -16,6 +17,12 @@ const mapDispatchToProps = (dispatch) => {
 
 class Company extends Component {
 
+  static defaultProps = {
+    company_name: 'Example Company',
+    contact: '1(800)000-000',
+    salary: '$75000'
+  }
+
   componentDidMount(){
     if (localStorage.getItem("token")) {
       this.getUser()
@@ -26,11 +33,52 @@ class Company extends Component {
     this.props.get_current_user()
   }
 
+  renderCompanies = () => {
+    if (this.props.current_user.current_user) {
+      if (this.props.current_user.current_user.companies.length === 1) {
+        return <li>a</li>
+      } else if(this.props.current_user.current_user.companies.length > 1) {
+        return this.props.current_user.current_user.companies.map( (company, index) => {
+          return (
+            <div key={index} className="card">
+              <div className="card-body">
+                <h5 className="card-title">{company.company_name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{company.contact}</h6>
+                <p className="card-text">{company.salary}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+    } else {
+      return (
+        <div>
+          <div>
+            <h2>You must be logged in to see your applications.</h2>
+            <h2>Example</h2>
+          </div>
+          <div className="exampleCompany">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{this.props.company_name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{this.props.contact}</h6>
+                <p className="card-text">{this.props.salary}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+
   render() {
-    console.log(this.props.current_user);
+    console.log(this.props.current_user.current_user);
     return (
       <div>
-        Company
+        Companies that you have applied to
+        <div className="displayCompanies">
+          {this.renderCompanies()}
+        </div>
       </div>
     )
   }
